16c63f05d540ce01d3bc79a9e896387b0e50db81
Sandro nadiradze <nadiradzesandro98@gmail.com>
2022-12-26 08:00:14 +0400

    Fixed navbar bug, navbar links were not disapearing from screen after user clicked them in an active hamburger menu

--- static/js/base.js
+++ static/js/base.js
@@ -6,8 +6,8 @@ let fourthLine = document.querySelector(".hamburger-4");
 let navbar = document.querySelector(".navbar");
 let leftNavLinks = document.querySelector(".left-links");
 let rightNavLinks = document.querySelector(".right-links");
+let primaryNavLinks = document.querySelectorAll(".nav-link-primary");
 const hamburgerMenu = document.querySelector(".hamburger-div");
-
 
 function shrinkHamburger() {
     secondLine.classList.add("shrink-second-line");
@@ -20,42 +20,54 @@ function unshrinkHamburger() {
     fourthLine.classList.remove("shrink-fourth-line");
 }
 hamburgerMenu.addEventListener("click", function () {
-    
     if (navbar.classList.contains("burger-clicked")) {
-        unshrinkHamburger()
+        unshrinkHamburger();
         navbar.classList.remove("burger-clicked");
         leftNavLinks.classList.add("hidden");
         rightNavLinks.classList.add("hidden");
-        
-        leftNavLinks.style.display = "none"
-        rightNavLinks.style.display = "none"
+
+        leftNavLinks.style.display = "none";
+        rightNavLinks.style.display = "none";
     } else {
-        shrinkHamburger()
+        shrinkHamburger();
         navbar.classList.add("burger-clicked");
         leftNavLinks.classList.remove("hidden");
-        rightNavLinks.classList.remove("hidden")
-        
-        leftNavLinks.style.display = "flex"
-        rightNavLinks.style.display = "flex"
+        rightNavLinks.classList.remove("hidden");
+
+        leftNavLinks.style.display = "flex";
+        rightNavLinks.style.display = "flex";
     }
 });
 
-
 window.addEventListener("resize", function () {
-    
     if (window.innerWidth > 1100) {
+        unshrinkHamburger();
         navbar.classList.remove("burger-clicked");
         leftNavLinks.classList.remove("hidden");
-        rightNavLinks.classList.remove("hidden")
-        leftNavLinks.style.display = "flex"
-        rightNavLinks.style.display = "flex"
+        rightNavLinks.classList.remove("hidden");
+        leftNavLinks.style.display = "flex";
+        rightNavLinks.style.display = "flex";
     } else {
         if (!navbar.classList.contains("burger-clicked")) {
             navbar.classList.remove("burger-clicked");
             leftNavLinks.classList.add("hidden");
             rightNavLinks.classList.add("hidden");
-            leftNavLinks.style.display = "none"
-            rightNavLinks.style.display = "none"
+            leftNavLinks.style.display = "none";
+            rightNavLinks.style.display = "none";
         }
     }
-})
+});
+
+for (let i = 0; i < 3; i++) {
+    primaryNavLinks[i].addEventListener('click', function () {
+        if (navbar.classList.contains("burger-clicked")) {
+            unshrinkHamburger();
+            navbar.classList.remove("burger-clicked");
+            leftNavLinks.classList.add("hidden");
+            rightNavLinks.classList.add("hidden");
+    
+            leftNavLinks.style.display = "none";
+            rightNavLinks.style.display = "none";
+        } 
+    });
+}
