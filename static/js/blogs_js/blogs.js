eb658762c57d40cd2af9d6d4375b8a2ecf8302c3
Sandro nadiradze <nadiradzesandro98@gmail.com>
2023-01-23 02:58:01 +0400

    Changed the implementation of masonry style layout

--- static/js/blogs_js/blogs.js
+++ static/js/blogs_js/blogs.js
@@ -1,63 +1,29 @@
 const blogsContainer = document.querySelector(".blogs-container");
-let blogs = document.querySelectorAll(".masonry_item");
-let previousScreenSize = window.innerWidth;
 let likeButtons = document.querySelectorAll(".like-button");
-
-function masonryGrid(columns, blogs) {
-    let columnWrappers = {};
-    for (let i = 0; i < columns; i++) {
-        columnWrappers[`column${i}`] = [];
-    }
-
-    for (let i = 0; i < blogs.length; i++) {
-        const column = i % columns;
-        columnWrappers[`column${column}`].push(blogs[i]);
-    }
-
-    for (let i = 0; i < columns; i++) {
-        let columnBlogs = columnWrappers[`column${i}`];
-        let div = document.createElement("div");
-        div.classList.add("column");
-
-        columnBlogs.forEach((blog) => {
-            div.appendChild(blog);
-        });
-
-        blogsContainer.appendChild(div);
-    }
-
-    let columnDiv = document.querySelectorAll(".column");
-
-    for (let i = 0; i < columnDiv.length; i++) {
-        if (columnDiv[i].childNodes.length === 0) {
-            columnDiv[i].remove();
-        }
-    }
-}
 
 window.addEventListener("resize", () => {
     if (window.innerWidth < 600 && previousScreenSize >= 600) {
-        masonryGrid(2, blogs);
+        blogsContainer.style.columns = "2";
     } else if (
         window.innerWidth >= 600 &&
         window.innerWidth < 1000 &&
         (previousScreenSize < 600 || previousScreenSize >= 1000)
     ) {
-        masonryGrid(3, blogs);
+        blogsContainer.style.columns = "3";
     } else if (
         window.innerWidth >= 1000 &&
         window.innerWidth < 1600 &&
         (previousScreenSize < 1000 || previousScreenSize >= 1600)
     ) {
-        masonryGrid(5, blogs);
+        blogsContainer.style.columns = "5";
     } else if (
         window.innerWidth >= 1600 &&
         window.innerWidth <= 1920 &&
         (previousScreenSize < 1600 || previousScreenSize >= 1920)
     ) {
-        masonryGrid(6, blogs);
+        blogsContainer.style.columns = "6";
     } else if (window.innerWidth > 1920 && previousScreenSize <= 1920) {
-        masonryGrid(7, blogs);
+        blogsContainer.style.columns = "7";
     }
 
     previousScreenSize = window.innerWidth;
@@ -65,15 +31,15 @@ window.addEventListener("resize", () => {
 
 function initialMasonry() {
     if (window.innerWidth < 600) {
-        masonryGrid(2, blogs);
+        blogsContainer.style.columns = "2";
     } else if (window.innerWidth >= 600 && window.innerWidth < 1000) {
-        masonryGrid(3, blogs);
+        blogsContainer.style.columns = "3";
     } else if (window.innerWidth >= 1000 && window.innerWidth < 1600) {
-        masonryGrid(5, blogs);
+        blogsContainer.style.columns = "5";
     } else if (window.innerWidth >= 1600 && window.innerWidth <= 1920) {
-        masonryGrid(6, blogs);
+        blogsContainer.style.columns = "6";
     } else {
-        masonryGrid(7, blogs);
+        blogsContainer.style.columns = "7";
     }
 }
 
