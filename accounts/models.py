93578db5bf1fa032fe922280a5e5440569510ad1
Sandro nadiradze <nadiradzesandro98@gmail.com>
2023-02-09 18:25:05 +0400

    Implemented a new method in the accounts application that helps me to present user profile pictures when they first sign up with social accounts, previously when users created an account with their GitHub or Google social accounts, their profile pictures were not displayed and instead, the default one was being presented so with this new method I am checking if the user has social accounts, if they have either google or GitHub accounts I display profile pictures from these social accounts, additionally, if the user decides to change their profile picture instead of displaying the social account profile picture we will display the new profile picture that the user uploaded and if the user deletes this new picture we will still be displaying the social account picture instead of default user picture

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import AbstractUser
 from imagekit.models import ProcessedImageField
 from imagekit.processors import ResizeToCover
+from allauth.socialaccount.models import SocialAccount
 
 
 class CustomUser(AbstractUser):
@@ -18,3 +19,36 @@ class CustomUser(AbstractUser):
         if not self.profile_picture:
             self.profile_picture = "profile_pictures/default_profile_picture.jpg"
         super().save(*args, **kwargs)
+
+    def get_profile_picture(self):
+
+        """
+        Function created to return the profile picture URL for a user, profile picture will be either from their social account or from media folder.
+
+        If user has signed up with google or github social accounts and they have not yet changed their profile picture, this function
+        will return their social account profile picture URL from the first linked social account. For github account it gets the picture URL from
+        extra_data['avatar_url'] and for google it gets the image from extra_data['picture'].
+
+        But if the user has not signed up with social account this function will return either the default picture URL or the URL of a picture that
+        the user has uploaded by themselves.
+
+        Returns:
+            str: The profile picture URL.
+        """
+        social_accounts = self.socialaccount_set.all()
+
+        if (
+            social_accounts
+            and self.profile_picture == "profile_pictures/default_profile_picture.jpg"
+        ):
+            social_account = social_accounts[0]
+
+            if social_account.provider == "github":
+                social_account_picture = social_account.extra_data["avatar_url"]
+            else:
+                social_account_picture = social_account.extra_data["picture"]
+
+            return social_account_picture
+
+        else:
+            return self.profile_picture.url
