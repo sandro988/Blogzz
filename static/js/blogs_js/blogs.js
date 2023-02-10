a11bda259aba2a2059665756cce02ddd0019a0e1
Sandro nadiradze <nadiradzesandro98@gmail.com>
2023-02-10 19:02:59 +0400

    Created a loader animation for a home page that shows on either page load or when users scroll down and infinite scrolling functionality is triggered.

--- static/js/blogs_js/blogs.js
+++ static/js/blogs_js/blogs.js
@@ -1,4 +1,9 @@
 const blogsContainer = document.querySelector(".blogs-container");
+const infiniteScrollUrl = "/home/?page=";
+let masonryBlogsLoader = document.querySelector(".masonry-blog-loader");
+let loadingIndicator = document.querySelector(
+    ".masonry-blog-loader-infinite-scroll"
+);
 let masonry;
 let blogThumbnailImages = document.querySelectorAll(".blog-thumbnail-img");
 let masonryItems = document.querySelectorAll(".masonry-item");
@@ -30,7 +35,7 @@ function initialMasonryColumnSizes() {
         } else if (window.innerWidth >= 1600 && window.innerWidth <= 1920) {
             return blogsContainer.offsetWidth / 6 - paddings;
         } else if (window.innerWidth > 1920) {
-            return blogsContainer.offsetWidth / 7 - paddings;
+            return blogsContainer.offsetWidth / 7 - paddings - 10;
         }
     }
 }
@@ -66,23 +71,42 @@ function initialMasonryTileSizes() {
     }
 }
 
+function onAlways() {
+    setTimeout(() => {
+        loadingIndicator.style.display = "none";
+    }, "500")
+    masonryBlogsLoader.style.display = "none";
+    blogsContainer.classList.remove("is-loading");
+
+    initialMasonryTileSizes();
+    masonry = new Masonry(blogsContainer, {
+        itemSelector: ".masonry-item",
+        horizontalOrder: true,
+        fitWidth: true,
+        gutter: 20,
+        transitionDuration: ".5s",
+        columnWidth: initialMasonryColumnSizes(),
+        stagger: 30,
+    });
+}
+
 window.addEventListener("load", () => {
     let loadThumbnails = imagesLoaded(blogThumbnailImages);
 
-    function onAlways() {
-        initialMasonryTileSizes();
-        masonry = new Masonry(blogsContainer, {
-            itemSelector: ".masonry-item",
-            horizontalOrder: true,
-            fitWidth: true,
-            gutter: 15,
-            transitionDuration: ".5s",
-            columnWidth: initialMasonryColumnSizes(),
-            stagger: 30,
-        });
-    }
+    blogsContainer.classList.add("is-loading");
+    masonryBlogsLoader.style.display = "flex";
+    loadingIndicator.style.display = "none";
 
     loadThumbnails.on("done", onAlways);
+});
+
+document.addEventListener("htmx:beforeRequest", (e) => {
+    // In this event listener we are checking if the url where the HTMX request was sent starts with '/home/?page='
+    // and if it is we will show the loader and after the masonry is initialized with new blogs we hide the loader.
+
+    if (e.target.getAttribute("hx-get").startsWith(infiniteScrollUrl)) {
+        loadingIndicator.style.display = "flex";
+    }
 });
 
 document.addEventListener("htmx:afterRequest", (e) => {
@@ -101,20 +125,6 @@ document.addEventListener("htmx:afterRequest", (e) => {
     masonryItems = document.querySelectorAll(".masonry-item");
     blogThumbnailImages = document.querySelectorAll(".blog-thumbnail-img");
     let loadThumbnails = imagesLoaded(blogThumbnailImages);
-
-    function onAlways() {
-        initialMasonryTileSizes();
-        masonry = new Masonry(blogsContainer, {
-            itemSelector: ".masonry-item",
-            horizontalOrder: true,
-            fitWidth: true,
-            gutter: 15,
-            transitionDuration: ".5s",
-            columnWidth: initialMasonryColumnSizes(),
-            stagger: 30,
-        });
-    }
-
     loadThumbnails.on("done", onAlways);
 });
 
